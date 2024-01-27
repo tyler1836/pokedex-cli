6cb2634ab6c7f973558a9ce0005da7be97061e3c
unknown <tyler.r.roll@gmail.com>
2024-01-26 19:34:23 -0700

    seperation for cleaner code

---
+++ commands/commands.go
@@ -0,0 +1,58 @@
+package commands
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+type commandline struct {
+	name string
+	description string
+	callback func() error
+}
+
+var commands map[string]commandline
+
+func commandHelp() error {
+	fmt.Printf("Available commands:\n\n")
+	for _, cmd := range commands {
+		fmt.Printf("%s:\n %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+func commandExit() error {
+	fmt.Println("Goodbye")
+	os.Exit(0)
+	return nil
+}
+
+func startPokedex() {
+	commands = map[string]commandline{
+		"help": commandline{
+			name: "help",
+			description: "Displays help message",
+			callback: commandHelp,
+		},
+		"exit": commandline{
+			name: "exit",
+			description: "Exit the pokedex",
+			callback: commandExit,
+		},
+	}
+
+	reader := bufio.NewReader(os.Stdin) 
+	
+	for {
+		fmt.Println("Pokedex <")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		switch input {
+			case "help":
+				commands["help"].callback()
+			case "exit":
+				commands["exit"].callback() 
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,61 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"bufio"
-	"os"
-	"strings"
+	pokedex "github.com/tyler1836/pokedex-cli/commands"
 )
 
-type commandline struct {
-	name string
-	description string
-	callback func() error
-}
-
-var commands map[string]commandline
-
-func commandHelp() error {
-	fmt.Printf("Available commands:\n\n")
-	for _, cmd := range commands {
-		fmt.Printf("%s:\n %s\n", cmd.name, cmd.description)
-	}
-	return nil
-}
-func commandExit() error {
-	fmt.Println("Goodbye")
-	os.Exit(0)
-	return nil
-}
 
 
 func main() {
 
-	commands = map[string]commandline{
-		"help": commandline{
-			name: "help",
-			description: "Displays help message",
-			callback: commandHelp,
-		},
-		"exit": commandline{
-			name: "exit",
-			description: "Exit the pokedex",
-			callback: commandExit,
-		},
-	}
-
-	reader := bufio.NewReader(os.Stdin) 
-	
-	for {
-		fmt.Println("Pokedex <")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-		switch input {
-			case "help":
-				commands["help"].callback()
-			case "exit":
-				commands["exit"].callback() 
-		}
-	}
+	pokedex.startPokedex()
 	
 }
